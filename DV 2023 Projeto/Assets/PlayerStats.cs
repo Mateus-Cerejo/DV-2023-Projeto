4dd411166c4aaf380d0a5eaefd0023a7ed660f0e
Mateus-Cerejo <202100250@estudantes.ips.pt>
2023-07-13 19:44:35 +0100

    City e animações

---
+++ DV 2023 Projeto/Assets/PlayerStats.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    private int actionsStat;
+    private int strengthStat;
+    private int atackSpeedStat;
+    private int speedStat;
+    private int luckStat;
+
+    private void Start()
+    {
+        actionsStat = PlayerPrefs.GetInt("actionsStat");
+        strengthStat = PlayerPrefs.GetInt("strengthStat");
+        atackSpeedStat = PlayerPrefs.GetInt("atackSpeedStat");
+        speedStat = PlayerPrefs.GetInt("speedStat");
+        luckStat = PlayerPrefs.GetInt("luckStat");
+
+    }
+
+    public int getActionsStat() { return actionsStat; }
+    public int getStrenghtStat() { return strengthStat; }
+    public int getAtackSpeedStat() { return atackSpeedStat; }
+    public int getSpeedtat() { return speedStat; }
+    public int getLuckStat() { return luckStat; }
+}
