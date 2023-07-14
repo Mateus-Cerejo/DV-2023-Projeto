eee147cf81e9df8b2b5bd0849edaf5871157ed35
Rmoura22 <202003000@estudantes.ips.pt>
2023-07-14 22:58:10 +0100

    Mais Animações

--- DV 2023 Projeto/Assets/OtherScripts/CameraBehaviour.cs
+++ DV 2023 Projeto/Assets/OtherScripts/CameraBehaviour.cs
@@ -9,6 +9,7 @@ public class CameraBehaviour : MonoBehaviour
     [SerializeField] private GameObject mainCamera;
     [SerializeField] private GameObject pcCamera;
     [SerializeField] private GameObject pcCamera_2;
+    [SerializeField] private GameObject pcCamera_3;
 
     // Update is called once per frame
     void Update()
@@ -18,18 +19,28 @@ public class CameraBehaviour : MonoBehaviour
             mainCamera.SetActive(true);
             pcCamera.SetActive(false);
             pcCamera_2.SetActive(false);
+            pcCamera_3.SetActive(false);
         }
         else if (Input.GetButtonDown("Cam2"))
         {
             mainCamera.SetActive(false);
             pcCamera.SetActive(true);
             pcCamera_2.SetActive(false);
+            pcCamera_3.SetActive(false);
         }
         else if (Input.GetButtonDown("Cam3"))
         {
             mainCamera.SetActive(false);
             pcCamera.SetActive(false);
             pcCamera_2.SetActive(true);
+            pcCamera_3.SetActive(false);
+        }
+        else if (Input.GetButtonDown("Cam4"))
+        {
+            mainCamera.SetActive(false);
+            pcCamera.SetActive(false);
+            pcCamera_2.SetActive(false);
+            pcCamera_3.SetActive(true);
         }
 
     }
