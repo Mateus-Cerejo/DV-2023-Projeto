5af5bfcf946c42023134b79b2561ad699852b346
Tiago Alexandre dos Santos Rosa <80277096+Tirro99@users.noreply.github.com>
2023-07-25 22:16:10 +0100

    Commit

--- DV 2023 Projeto/Assets/Entities/Enemies/Brute/Scripts/BruteStats.cs
+++ DV 2023 Projeto/Assets/Entities/Enemies/Brute/Scripts/BruteStats.cs
@@ -10,7 +10,11 @@ public class BruteStats : MonoBehaviour
 
     [SerializeField] private int curHealth;
     [SerializeField] private int zombiesCount;
-    // Start is called before the first frame update
+
+
+    [SerializeField] private GameObject[] artifacts;
+    [SerializeField] private ArtifactBackPack abp;
+
     void Start()
     {
         animator = GetComponent<Animator>();
@@ -31,6 +35,35 @@ public class BruteStats : MonoBehaviour
 
         gameEvents.InvokeEnemyDied(zombiesCount);
 
+        spawnArtifact();
+
         Destroy(gameObject);
+    }
+
+    private void spawnArtifact()
+    {
+        int spawnArtifact = Mathf.RoundToInt(Random.value);
+        if (spawnArtifact == 1)
+        {
+            if (abp.iceAuraArtifactQuantityStored < 1)
+            {
+                float artifactToSpawn = Random.Range(0, 89);
+                if (artifactToSpawn < 25) Instantiate(artifacts[0], transform.position, Quaternion.identity);
+                if (artifactToSpawn >= 25 && artifactToSpawn < 50) Instantiate(artifacts[1], transform.position, Quaternion.identity);
+                if (artifactToSpawn >= 50 && artifactToSpawn < 75) Instantiate(artifacts[2], transform.position, Quaternion.identity);
+                if (artifactToSpawn >= 75 && artifactToSpawn < 85) Instantiate(artifacts[3], transform.position, Quaternion.identity);
+                if (artifactToSpawn >= 85 && artifactToSpawn < 87) Instantiate(artifacts[4], transform.position, Quaternion.identity);
+                if (artifactToSpawn >= 87) Instantiate(artifacts[5], transform.position, Quaternion.identity);
+            }
+            else
+            {
+                float artifactToSpawn = Random.Range(0, 87);
+                if (artifactToSpawn < 25) Instantiate(artifacts[0], transform.position, Quaternion.identity);
+                if (artifactToSpawn >= 25 && artifactToSpawn < 50) Instantiate(artifacts[1], transform.position, Quaternion.identity);
+                if (artifactToSpawn >= 50 && artifactToSpawn < 75) Instantiate(artifacts[2], transform.position, Quaternion.identity);
+                if (artifactToSpawn >= 75 && artifactToSpawn < 85) Instantiate(artifacts[3], transform.position, Quaternion.identity);
+                if (artifactToSpawn >= 85) Instantiate(artifacts[4], transform.position, Quaternion.identity);
+            }
+        }
     }
 }
