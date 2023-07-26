1ca750e935f916b17ab6a642c3616fdc580c2a39
Rmoura22 <202003000@estudantes.ips.pt>
2023-07-26 20:48:59 +0100

    Barreiras aparecem com o nivel da Factory

---
+++ DV 2023 Projeto/Assets/World/3D Map/BarriersManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarriersManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string chosenBarrierName = "Level" + PlayerPrefs.GetInt("Factory");
+        transform.Find(chosenBarrierName).gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
